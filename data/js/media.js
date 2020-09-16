9feea16620e204feb15019218dea4f6fcca4bf5e
Dakumisu <56684844+Dakumisu@users.noreply.github.com>
2020-09-16 15:44:12 +0200

    ueue

--- data/js/media.js
+++ data/js/media.js
@@ -5,7 +5,7 @@ $p4 = document.getElementById("#Polygone_5");
 $p5 = document.getElementById("#Polygone_6");
 $p6 = document.getElementById("#Polygone_7");
 
-document.getElementById('#Polygone_2').onclick = p1; 
+$p1 = document.getElementById('#Polygone_2').onclick; 
 
 $p1.addEventListener('click', event => {
     alert(`Nombre de clics : ${event.detail}`);
