5e91b9c589ba2564a8fd3af4eb88448f72ab6876
Dakumisu <alex.gattefosse@gmail.com>
2020-09-18 20:19:43 +0200

    j'ai fini le menu normalement

--- data/js/media.js
+++ data/js/media.js
@@ -1,18 +1,42 @@
-// $p1 = document.getElementById("#Polygone_2");
-$p2 = document.getElementById("#Polygone_3");
-$p3 = document.getElementById("#Polygone_4");
-$p4 = document.getElementById("#Polygone_5");
-$p5 = document.getElementById("#Polygone_6");
-$p6 = document.getElementById("#Polygone_7");
+// $p1 = document.getElementById("#Poly_2");
+// $p2 = document.getElementById("#Poly_3");
+// $p3 = document.getElementById("#Poly_4");
+// $p4 = document.getElementById("#Poly_5");
+// $p5 = document.getElementById("#Poly_6");
+// $p6 = document.getElementById("#Poly_7");
 
 
 $turn = 0;
 
 $(document).ready(function () {
   $("#turn").click(function () {
-    $turn = $turn + 60;
+    $turn = $turn + -60;
     console.log($turn);
     $("svg").css({"transform" : "rotateZ("+$turn+"deg)"});
+  });
+
+  $("#Poly_2").click(function () {
+    console.log("MÉDIAS CLASSIQUES");
+  });
+
+  $("#Poly_3").click(function () {
+    console.log("FOURNISSEURS D'ACCÈS");
+  });
+
+  $("#Poly_4").click(function () {
+    console.log("CONTENUS CULTURELS");
+  });
+
+  $("#Poly_5").click(function () {
+    console.log("ÉCONOMIE DES MÉDIAS");
+  });
+
+  $("#Poly_6").click(function () {
+    console.log("RÉSEAUX SOCIAUX");
+  });
+
+  $("#Poly_7").click(function () {
+    console.log("RÉGULATION DES MÉDIAS");
   });
 
   // $(document).scroll(function () {
