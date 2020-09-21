3df5c9b48172a8dface8f264feed113534495b35
Dakumisu <56684844+Dakumisu@users.noreply.github.com>
2020-09-21 11:44:09 +0200

    nsm bazet

--- data/js/media.js
+++ data/js/media.js
@@ -8,33 +8,33 @@ $(document).ready(function () {
   });
 
   $("#Poly_2").click(function () {
-    console.log("MÉDIAS CLASSIQUES");
+    // console.log("MÉDIAS CLASSIQUES");
     $("svg").css({"opacity" : "0"});
   });
 
   $("#Poly_3").click(function () {
-    console.log("FOURNISSEURS D'ACCÈS");
+    // console.log("FOURNISSEURS D'ACCÈS");
+    $("svg").css({"opacity" : "0"});
   });
 
   $("#Poly_4").click(function () {
-    console.log("CONTENUS CULTURELS");
+    // console.log("CONTENUS CULTURELS");
+    $("svg").css({"opacity" : "0"});
   });
 
   $("#Poly_5").click(function () {
-    console.log("ÉCONOMIE DES MÉDIAS");
+    // console.log("ÉCONOMIE DES PLATEFORMES");
+    $("svg").css({"opacity" : "0"});
   });
 
   $("#Poly_6").click(function () {
-    console.log("RÉSEAUX SOCIAUX");
+    // console.log("RÉSEAUX SOCIAUX");
+    $("svg").css({"opacity" : "0"});
   });
 
   $("#Poly_7").click(function () {
-    console.log("RÉGULATION DES MÉDIAS");
+    // console.log("RÉGULATION DES MÉDIAS");
+    $("svg").css({"opacity" : "0"});
   });
       
 });
-
-
-test = function maFonction() {
-  $('body').css('background-color','green');
-}
