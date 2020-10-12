5ad00252768d8465d76f82bc07b6e53b41e452bf
Dakumisu <56684844+Dakumisu@users.noreply.github.com>
2020-10-12 08:15:35 +0200

    ue

--- SONY/data/js/app.js
+++ SONY/data/js/app.js
@@ -1,40 +1,50 @@
-import Swup from 'swup';
-const swup = new Swup();
+// import Swup from 'swup';
+// const swup = new Swup();
+// import Scrollbar from 'smooth-scrollbar';
+// Scrollbar.init(document.querySelector('#my-scrollbar'));
 
-// window.onload = () => {
-//     window.addEventListener("scroll", () => {
-//         let taille = document.documentElement.scrollHeight - window.innerHeight;
-//         let position = window.scrollY;
-//         let largeur = document.documentElement.clientWidth;
-//         let barre = position / taille * largeur;
-//         document.getElementById("progress-bar").style.width = barre + "px";
-//         console.log(barre + "px");
+window.onload = () => {
+    for ($i=0; $i < document.getElementsByClassName("section").length; $i++) {
+        var progressSection$i = document.querySelector(".section-"+$i);
+        // console.log(progressSection$i);
+        var progressBarSection = progressSection$i.dataset.anchor;
+        console.log(progressBarSection);
+    }
 
-//         let ueue = document.getElementById("fullpage");
-//         console.log(ueue);
-//     });
+    // window.addEventListener("scroll", () => {
+    //     let taille = document.documentElement.scrollHeight - window.innerHeight;
+    //     let position = window.scrollY;
+    //     let largeur = document.documentElement.clientWidth;
+    //     let barre = position / taille * largeur;
+    //     document.getElementById("progress-bar").style.width = barre + "px";
+    //     console.log(barre + "px");
 
+    //     // let ueue = document.getElementById("fullpage");
+    //     // console.log(ueue);
+    // });
+
+    // window.addEventListener("mousemove", () => {
+    //     console.log("ueueue");
+    //     if (progressBarSection == "progress0") {
+    //         document.getElementById("progress-bar").style.width = 0 + "px";
+    //     } else if (progressBarSection == "progress1") {
+    //         document.getElementById("progress-bar").style.width = 50 + "px";
+    //     }
+    // })
+
+
+
+
+
+    // let progressue = progressSection.getAttribute("anchor");
+    // console.log(progressue);
 //     let taille = document.documentElement.scrollHeight - window.innerHeight;
 //     let position = window.scrollY;
 //     let largeur = document.documentElement.clientWidth;
 //     let barre = position / taille * largeur;
 
-//     if (anchors == "progress1") {
-//         document.getElementById("progress-bar").style.width = 50 + "px";
-//     }
 
 
 
-// }
 
-// $(function () {
-//     $(document).on('scroll', function () {
-//         let hauteur = $(document).height() - $(window).height()
-//         let position = $(document).scrollTop()
-//         let largeur = $(window).width()
-//         let barre = position / hauteur * largeur
-
-//         $("#progress-bar").css("width", barre)
-//         console.log(barre + "px");
-//     });
-// });
+}
