eed3aa0bba4ec4eb3329db7395ed91eebbf1295f
Dakumisu <alex.gattefosse@gmail.com>
2020-11-12 11:15:01 +0100

    ueue

---
+++ data/js/media.js
@@ -0,0 +1,40 @@
+$turn = 0;
+
+$(document).ready(function () {
+  $("#turn").click(function () {
+    $turn = $turn + -60;
+    console.log($turn);
+    $("svg").css({"transform" : "rotateZ("+$turn+"deg)"});
+  });
+
+  $("#Poly_2").click(function () {
+    console.log("MÉDIAS CLASSIQUES");
+    $("#Polygone_2").css({"transform" : "scale(22.5)"});
+  });
+
+  $("#Poly_3").click(function () {
+    console.log("FOURNISSEURS D'ACCÈS");
+  });
+
+  $("#Poly_4").click(function () {
+    console.log("CONTENUS CULTURELS");
+  });
+
+  $("#Poly_5").click(function () {
+    console.log("ÉCONOMIE DES MÉDIAS");
+  });
+
+  $("#Poly_6").click(function () {
+    console.log("RÉSEAUX SOCIAUX");
+  });
+
+  $("#Poly_7").click(function () {
+    console.log("RÉGULATION DES MÉDIAS");
+  });
+      
+});
+
+
+test = function maFonction() {
+  $('body').css('background-color','green');
+}
